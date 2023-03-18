dfab1bcfc157d3e58f467e4b0121e013abc0a557
mohamad <mohammad.pr285@gmail.com>
2023-03-18 12:37:31 -0700

    getTime function defined and tested on branch jsGetTime

---
+++ dClock.js
@@ -0,0 +1,34 @@
+function getTime(){
+
+     // defining variable(s)
+    var ND = new Date();
+    var hours = ND.getHours();
+    var minutes = ND.getMinutes();
+    var seconds = ND.getSeconds();
+    var PA = "AM";
+
+    // turning 00 to 12 AM
+    if (hours == 0){
+        hours = 12;
+    }
+
+    // changing PM and AM, 24-12
+    if (hours > 12){
+        hours -= 12;
+        PA = "PM";
+    }
+    // adding 0 to numbers lower than 10
+    if (hours < 10){
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    // adding everything together
+    var Time = hours + ":" + minutes + ":" + seconds + PA;
+}
